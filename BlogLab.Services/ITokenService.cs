2c2e0636b2b3c9481b2e0811295c2b1b7914e34d
Automatrio <daniel.baisi.hadad@gmail.com>
2021-01-19 15:20:24 -0300

    Tokens and stuff.

---
+++ BlogLab.Services/ITokenService.cs
@@ -0,0 +1,11 @@
+using System;
+using BlogLab.Models.Account;
+
+namespace BlogLab.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+        
+    }
+}
